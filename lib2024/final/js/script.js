0b5f9e918807bf64028012e6c67b4f3b8696506f
four0206 <59583856+four0206@users.noreply.github.com>
2020-04-05 19:02:15 -0400

    edited text

--- lib2024/final/js/script.js
+++ lib2024/final/js/script.js
@@ -4,14 +4,14 @@ function init() {
 
 	// Code for the JQuery function attached to the submit event of the form with the id "form"
 	$('#form').submit(function (e) {
-		// e.preventDefualt is to avoid the form being submitted to the page specified in the action attribute 
+		// e.preventDefault avoids the form being submitted to the page specified in the action attribute 
    		 e.preventDefault();
-   		 // This code is for passing the current form (this) to variable form 
+   		 // This code is for passing the current form (this) to the variable form 
    		 $("#fresults").removeClass('reveal');
    		 var form = this;
    		 // fadeIn is a jQuery function to fadeIn an element 
    		 $(".overlay-container").fadeIn(1000, function(){
-   		 		//This code is to call the showFormValues function and pass variable form to it as an argument
+   		 		//This code is to call the showFormValues function and pass the variable form to it as an argument
    		 		showformValues(form);
 		   		 // fadeOut is a jQuery function to fadeOut an element 
    		 		$('.overlay-container').delay(500).fadeOut(500);
@@ -21,19 +21,19 @@ function init() {
 
 }
 
-// This code is to show the form values in the results div with the argument "form"
+// This code is to show the form values in the fresults div with the argument "form"
 function showformValues(form){
 	//serializeArray is a jQuery function used to get the values of a form as a js object
 	var formValues = $(form).serializeArray(); 
-	// $.each is a jQQuery alternative to the 'for' loop to iterate through a JS array or object; this is especially beneficial when the length of array is not known
-	// index is the index  of the current element i.e 0,1,2,3 so on 
-		//field is the actual field being accessed 
+	// $.each is a jQuery alternative to the 'for' loop to iterate through a JS array or object; this is especially beneficial when the length of array is not known
+	// index is the index  of the current element i.e 0,1,2,3 etc. 
+		//'field' is the actual field being accessed 
 		
 	$.each(formValues, function(index, field){
 
 		// This code does the following: 
 		// 1) $("#results") -- (Gets the/selects the div with the id 'fresults')
-		// 2) .fund("#"+field.name+"_result") -- finds the element with the id equal to the name of the field being accessed along with text ("_result") Eg : name, pc_result, email_result
+		// 2) .fund("#"+field.name+"_result") -- finds the element with the id equal to the name of the field being accessed along with text ("_result") Eg : name_result, postal_result, email_result
 		// 3) Modifies the text inside the selected element and replaces it with the value of this field   
 		$("#fresults").find("#"+field.name+"_result").text(field.value);
 
